0f05914ed69b18d7318232479ac87e35fa1eeb36
Phan Vũ Tuấn Anh <88922348+MarcoPhanVu@users.noreply.github.com>
2023-06-21 01:51:38 +0700

    enhance transactionID generator, incrememnt idCount if transactionID existed

--- servers/alter_all.php
+++ servers/alter_all.php
@@ -1,20 +1,15 @@
 <?php include "../components/header_server.php" ?>
 
 <?php
-	session_start();
-	// echo $_SESSION["session_variable"];
-
 	$field_query = "SELECT * FROM categories";
 	$field_query_result = mysqli_query($connection, $field_query);
-	// print_r($field_query_result);
 	$field_list = mysqli_fetch_all($field_query_result, MYSQLI_ASSOC);
-	// print_r($field_list);
 
 
-
-	// PROCESS FORM DATA
+	// PROCESS TRANSACTION FORM DATA
 	$transWallet_Data = $transCategory_Data = $transAmount_Data = $transDetails_Data = $transDate_Data = "";
 	$transWallet_ErrMsg = $transCategory_ErrMsg = $transAmount_ErrMsg = $transDetails_ErrMsg = $transDate_ErrMsg = "";
+
 
 	if (isset($_POST["transactionCreated"])) {
 		echo "Transaction created heheheeh";
@@ -54,16 +49,38 @@
 
 
 		// Submit data
-		if (empty($transWallet_ErrMsg) && empty($transCategory_ErrMsg) && empty($transAmount_ErrMsg) && empty($transDetails_ErrMsg) && empty($transDate_ErrMsg)) {
-			// Submitted + No Error Message => proceed
+		if (empty($transWallet_ErrMsg) && empty($transCategory_ErrMsg) && empty($transAmount_ErrMsg) && empty($transDetails_ErrMsg) && empty($transDate_ErrMsg)) { // Submitted + No Error Message => proceed
+			
+			// CHECKING IF traqnsactionID exists
+				$IDCount = 0; // Will be incremented if a transactionID existed
 
-			$sql_statement = "INSERT INTO transactions (walletID, categoryID, amount, description, date) VALUES ('$transWallet_Data', '$transCategory_Data', '$transAmount_Data', '$transDetails_Data', '$transDate_Data')";
+				$getCurrentUser = mysqli_query($connection, 
+				"SELECT w.userID FROM wallet_category AS wc 
+				INNER JOIN wallets AS w 
+				ON w.walletID = wc.walletID 
+				WHERE w.walletID = '$transWallet_Data';");
 
-			echo "here: " . $sql_statement . "<br>";
+				$currentUser = mysqli_fetch_array($getCurrentUser); //
+				$processedDate = implode(explode("-", $transDate_Data)); // convert date from yyyy-mm-dd to yyyymmdd
+				$transactionID = $currentUser['userID'] . $processedDate . $IDCount; // Generating unique transactionID
+
+				$checkTransactionIDExisted = mysqli_query($connection, "SELECT transactionID FROM transactions WHERE transactionID = '$transactionID'");
+
+				while (mysqli_num_rows($checkTransactionIDExisted)) { // not 0 = true
+					$IDCount += 1;
+					$transactionID = $currentUser['userID'] . $processedDate . $IDCount;
+					$checkTransactionIDExisted = mysqli_query($connection, "SELECT transactionID FROM transactions WHERE transactionID = '$transactionID'");
+					echo "Incrementing transID to " . $IDCount . "<br>";
+				}
+
+
+
+			// Continue inserting
+			$sql_statement = "INSERT INTO transactions (transactionID, walletID, categoryID, amount, description, date) VALUES ('$transactionID', '$transWallet_Data', '$transCategory_Data', '$transAmount_Data', '$transDetails_Data', '$transDate_Data')";
+			echo "transSQLInsert here: " . $sql_statement . "<br>";
 
 			if (mysqli_query($connection, $sql_statement)) {
 				header($_SERVER["PHP_SELF"]);
-
 			} else {
 				echo $transWallet_Data . "<br>";
 				echo "Error: " . mysqli_error($connection);
@@ -74,9 +91,8 @@
 
 ?>
 
-<section class="alter_info">
-
-	<!-- Seclect db by using "SHOW TABLES"-->
+<!-- CATEGORIESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS -->
+<section class="alter_info" id="info_category">
 	<!-- Let's just do it in the normal way -->    
 	<div class="form-container">
 		<div class="title">Creating Transactions</div>
@@ -175,12 +191,111 @@
 		include "./display/display_data.php";
 		echo '</div>';
 	?>
-	
-	<button id="show-display-num">Click me to show num</button>
-	<button id="increase-num" onclick="">+1</button>
-	<button id="decrease-num" onclick="">-1</button>
 
-	<div id="display-num">1000002</div>
+</section>
+
+
+<!-- TRANSACTIONSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS -->
+<section class="alter_info hidden" id="info_transaction">
+
+	<!-- Let's just do it in the normal way -->    
+	<div class="form-container">
+		<div class="title">Creating Transactions</div>
+		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" id="createTransactionForm">
+			<!-- Select tag will be use later, now we use Radio for fast interaction  -->
+			<fieldset>
+				<legend>Choose your Wallets</legend>
+				<?php echo $transWallet_ErrMsg; ?>
+				<?php 
+					$wallet_query = "SELECT * FROM wallets";
+					$wallet_query_result = mysqli_query($connection, $wallet_query);
+					$wallet_data_all = mysqli_fetch_all($wallet_query_result, MYSQLI_ASSOC);
+
+					// print_r($wallet_data_all);
+					// echo "<br>" . "heh";
+					// echo "wallet_data_type: " . gettype($wallet_data_all) . "<br>";
+					foreach (array_values($wallet_data_all) as $wallet_data) {
+						$walletID = $wallet_data["walletID"];
+						echo    "<label for='$walletID'>
+									<input type='radio' name='transWallet' value='$walletID'>
+								$walletID</label>" . "";
+					}
+				?>
+			</fieldset>
+
+			<fieldset>
+				<legend>Choose your Category</legend>
+				<!-- GET CATEGORIES FROM USER + WALLET -->
+				<?php echo $transCategory_ErrMsg; ?>
+
+				<?php 
+					$category_query = "SELECT * FROM categories";
+					$category_query_result = mysqli_query($connection, $category_query);
+					$category_data_all = mysqli_fetch_all($category_query_result, MYSQLI_ASSOC);
+					
+					foreach (array_values($category_data_all) as $category_data) {
+						$categoryID = $category_data["categoryID"];
+						echo    "<label for='$categoryID'>
+									<input type='radio' name='transCategory' value='$categoryID'>
+								$categoryID</label>" . "";
+					}
+				?>
+			</fieldset>
+			<fieldset class="ext-info">
+				<legend>Additional Information</legend>
+
+				<?php echo $transAmount_ErrMsg; ?>
+				<label>
+					Amount:
+					<input type="number" name="transAmount" step="0.01" placeholder="160.00">
+				</label>
+
+				<?php echo $transDetails_ErrMsg; ?>
+				<label>
+					Details: 
+					<textarea name="transDetails" placeholder="Describe transaction here..."></textarea>
+				</label>
+
+				<?php echo $transDate_ErrMsg; ?>
+				<label>
+					Occured on:
+					<input type="date" name="transDate" id="transaction-date">
+				</label>
+
+			</fieldset>
+			
+			<!-- <p id="test-ele">Test value</p> -->
+
+			<div class="bottom-right">
+				<input class="btn" type="submit" name="transactionCreated" value="Create">
+			</div>
+		</form>
+	</div>
+
+	<div class="info-display-tab">
+		<?php 
+			// echo "<p>WalletId: " . $_POST["transWallet"] . "</p>"; 
+			// echo "<p>CategoryId: " . $_POST["transCategory"] . "</p>"; 
+			// echo "<p>Transaction Amount: " . $_POST["transAmount"] . "</p>"; 
+			// echo "<p>Transaction Details: " . $_POST["transDetails"] . "</p>"; 
+			// echo "<p>Transaction Date: " . $_POST["transDate"] . "</p>"; 
+			// echo "<p>Transaction created " . $_POST["transactionCreated"] . "</p>"; 
+
+			echo "<p>WalletId: " . $transWallet_Data . "</p>"; 
+			echo "<p>CategoryId: " . $transCategory_Data . "</p>"; 
+			echo "<p>Transaction Amount: " . $transAmount_Data . "</p>"; 
+			echo "<p>Transaction Details: " . $transDetails_Data . "</p>"; 
+			echo "<p>Transaction Date: " . $transDate_Data . "</p>"; 
+			echo "<p>Transaction created " . isset($_POST["transactionCreated"]) . "</p>"; 
+		?>
+	</div>
+
+	<?php
+		echo '<div class="">';
+		$target_table = "transactions";
+		include "./display/display_data.php";
+		echo '</div>';
+	?>
 
 </section>
 	
